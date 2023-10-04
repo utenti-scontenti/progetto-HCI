50a69085bddac858c31a389918777a99b17de047
darcros <darcros7@gmail.com>
2023-10-04 12:51:38 +0200

    fix missing path base

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,8 +1,12 @@
+<script>
+	import { base } from '$app/paths';
+</script>
+
 <h1 class="font-black text-9xl">Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
 
 <p>
-	<a href="/markdown-test">Go to markdown test page</a>
+	<a href="{base}/markdown-test">Go to markdown test page</a>
 </p>
 
 <button class="btn btn-primary">Button</button>
