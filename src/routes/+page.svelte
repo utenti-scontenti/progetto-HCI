1e7e634b356882185d856c9a3198a0fb6b3c26b2
darcros <darcros7@gmail.com>
2023-10-04 21:56:07 +0200

    homepage

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,32 +2,38 @@
 	import { base } from '$app/paths';
 </script>
 
-<!-- background -->
-<div class="-z-10 absolute top-[200px] right-[420px]">
-	<p
-		class="w-0 h-0 pointer-events-none text-[900px] leading-[0] font-extrabold text-[rgba(0,0,0,0.2)] rotate-45"
-	>
-		:(
-	</p>
-</div>
-
-<div class="mx-16 prose">
+<div class="mx-16 mt-8 mb-16 prose">
 	<p class="text-xl">
 		Fondamenti di <abbr title="Human-Computer Interaction">HCI</abbr> &ndash; a.a. 2023-24
 	</p>
+</div>
+
+<div class="mx-auto mb-32 prose relative">
+	<p
+		class="
+			pointer-events-none
+			-z-10 absolute right-0 bottom-0
+			m-0 p-0
+			text-[275px] font-extrabold text-[rgba(0,0,0,0.2)]
+			rotate-45 translate-y-20
+		"
+	>
+		:(
+	</p>
+
 	<p class="text-3xl font-bold mb-0">Gruppo</p>
 	<h1 class="text-9xl font-extrabold">Utenti<br />Scontenti</h1>
 </div>
 
-<div class="container mx-auto">
-	<div class="prose mx-auto">
-		<h2>Consegne</h2>
-		<ul>
-			<li>
-				<a href="{base}/consegne/compito1">Vai a consegna 1</a>
-			</li>
-		</ul>
-	</div>
+<div class="prose mx-auto">
+	<h2>Componenti</h2>
+	<p>Poi metteremo qui dei contenuti</p>
+	<h2>Consegne</h2>
+	<ul>
+		<li>
+			<a href="{base}/consegne/compito1">Vai a consegna 1</a>
+		</li>
+	</ul>
 </div>
 
 <style>
