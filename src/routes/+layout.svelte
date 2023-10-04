06d331887af25b34fb991dd348dc7cb7f7d16065
darcros <darcros7@gmail.com>
2023-10-04 22:41:11 +0200

    add navbar

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,28 @@
 <script>
 	import '@fontsource-variable/inter';
 	import '../app.css';
+
+	import { base } from '$app/paths';
+	import { page } from '$app/stores';
+
+	const pages = [
+		{ name: 'Consegna 1', routeId: '/consegne/compito1' },
+		{ name: 'Consegna 2', routeId: '/consegne/compito2' },
+		{ name: 'Consegna 3', routeId: '/consegne/compito3' }
+	];
 </script>
+
+<nav class="navbar bg-base-200">
+	<a class="btn btn-ghost normal-case text-xl font-bold" href="{base}/">Utenti Scontenti</a>
+
+	<!-- TODO: responsive -->
+	<ul class="menu menu-vertical lg:menu-horizontal bg-base-200 rounded-box">
+		{#each pages as p}
+			<li>
+				<a class:active={$page.route.id === p.routeId} href="{base}{p.routeId}">{p.name}</a>
+			</li>
+		{/each}
+	</ul>
+</nav>
 
 <slot />
