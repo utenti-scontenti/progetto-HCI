8931e81f2c4da8d80453558a007f22f49c8d2a1d
darcros <darcros7@gmail.com>
2023-10-04 12:20:12 +0200

    add tailwindcss

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,5 @@
 import adapter from '@sveltejs/adapter-static';
+import { vitePreprocess } from '@sveltejs/kit/vite';
 
 const dev = process.argv.includes('dev');
 
@@ -9,7 +10,8 @@ const config = {
 		paths: {
 			base: dev ? '' : process.env.BASE_PATH
 		}
-	}
+	},
+	preprocess: vitePreprocess()
 };
 
 export default config;
