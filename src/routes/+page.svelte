3d8a0ff5d50e328ab5183405cd9d51c89e2cc046
darcros <darcros7@gmail.com>
2023-10-05 10:51:13 +0200

    use theme color

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -8,7 +8,7 @@
 			pointer-events-none
 			-z-10 absolute right-0 bottom-0
 			m-0 p-0
-			text-[275px] font-extrabold text-[rgba(0,0,0,0.2)]
+			text-[275px] font-extrabold text-base-200
 			rotate-45 translate-y-20
 		"
 	>
