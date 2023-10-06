bf66553cf986d1e3c045d8fc7b45e40a69dcad03
darcros <darcros7@gmail.com>
2023-10-06 15:14:51 +0200

    add sidebar

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,6 +2,8 @@
 	import '@fontsource-variable/inter';
 	import '../app.css';
 
+	import { MenuIcon } from 'lucide-svelte';
+
 	import { base } from '$app/paths';
 	import { page } from '$app/stores';
 
@@ -12,31 +14,55 @@
 	];
 </script>
 
-<div class="min-h-screen flex flex-col">
-	<nav class="navbar bg-base-200">
-		<a class="btn btn-ghost normal-case text-xl font-bold" href="{base}/">Utenti Scontenti</a>
+<!-- drawer container -->
+<div class="drawer">
+	<input id="app-drawer" type="checkbox" class="drawer-toggle" />
 
-		<!-- TODO: responsive -->
-		<ul class="menu menu-vertical lg:menu-horizontal bg-base-200 rounded-box">
+	<!-- page content -->
+	<div class="drawer-content min-h-screen flex flex-col">
+		<!-- navbar -->
+		<nav class="navbar w-full bg-base-200">
+			<div class="lg:hidden">
+				<label for="app-drawer" aria-label="open sidebar" class="btn btn-square btn-ghost">
+					<MenuIcon />
+				</label>
+			</div>
+			<a class="btn btn-ghost normal-case text-xl font-bold" href="{base}/">Utenti Scontenti</a>
+			<ul class="menu hidden lg:menu-horizontal bg-base-200">
+				{#each pages as p}
+					<li>
+						<a class:active={$page.route.id === p.routeId} href="{base}{p.routeId}">{p.name}</a>
+					</li>
+				{/each}
+			</ul>
+		</nav>
+
+		<!-- content container -->
+		<div class="flex-grow">
+			<slot />
+		</div>
+
+		<!-- footer -->
+		<footer class="footer footer-center p-4 bg-base-300 text-base-content">
+			<aside>
+				<p>
+					Sito realizzato per il corso di <span class="font-bold"
+						>Fondamenti di <abbr title="Human-Computer Interaction">HCI</abbr> &ndash; a.a. 2023-24</span
+					>
+				</p>
+			</aside>
+		</footer>
+	</div>
+
+	<!-- drawe items -->
+	<div class="drawer-side">
+		<label for="app-drawer" aria-label="close sidebar" class="drawer-overlay" />
+		<ul class="menu p-4 w-80 min-h-full bg-base-200">
 			{#each pages as p}
 				<li>
 					<a class:active={$page.route.id === p.routeId} href="{base}{p.routeId}">{p.name}</a>
 				</li>
 			{/each}
 		</ul>
-	</nav>
-
-	<div class="flex-grow">
-		<slot />
 	</div>
-
-	<footer class="footer footer-center p-4 bg-base-300 text-base-content">
-		<aside>
-			<p>
-				Sito realizzato per il corso di <span class="font-bold"
-					>Fondamenti di <abbr title="Human-Computer Interaction">HCI</abbr> &ndash; a.a. 2023-24</span
-				>
-			</p>
-		</aside>
-	</footer>
 </div>
