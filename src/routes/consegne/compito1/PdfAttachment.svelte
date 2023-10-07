6fe1df4749cc13ee5f06d7ca0f7f2297412a23ea
darcros <darcros7@gmail.com>
2023-10-07 21:04:41 +0200

    fix text color

--- src/routes/consegne/compito1/PdfAttachment.svelte
+++ src/routes/consegne/compito1/PdfAttachment.svelte
@@ -21,10 +21,10 @@
 
 <div class="border-[1px] border-secondary-content rounded-2xl bg-base-300">
 	<!-- main -->
-	<div class="w-full p-4 flex flex-row justify-between items-center ">
-		<div class="flex flex-row items-center px-4 space-x-2">
+	<div class="w-full p-4 flex flex-row justify-between items-center">
+		<div class="text-base-content flex flex-row items-center px-4 space-x-2">
 			<FileIcon />
-			<p class="text-lg text-primary-content">{fileName}</p>
+			<p class="text-lg">{fileName}</p>
 		</div>
 		<a class="btn btn-ghost" href={src} download={fileName}><DownloadIcon />Download</a>
 	</div>
