5146b1140f356a711e0303f921edaa953807d0e8
darcros <darcros7@gmail.com>
2023-10-07 15:53:09 +0200

    new <PdfAttachment> component

--- src/routes/consegne/compito1/+page.svelte
+++ src/routes/consegne/compito1/+page.svelte
@@ -2,12 +2,14 @@
 	import { base } from '$app/paths';
 	import Prose from '$lib/Prose.svelte';
 
-	import Slides from './Slides.svelte';
 	import Content from './Content_01.md';
+	import PdfAttachment from './PdfAttachment.svelte';
 </script>
 
 <Prose>
 	<Content />
 </Prose>
 
-<Slides src="{base}/consegne/compito1/slides.pdf" fileName="slides.pdf" />
+<div class="container mx-auto my-8 px-8">
+	<PdfAttachment src="{base}/consegne/compito1/slides.pdf" fileName="slides.pdf" />
+</div>
