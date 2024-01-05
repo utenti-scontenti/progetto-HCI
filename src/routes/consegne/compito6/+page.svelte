3f3bf42bab468c62e3a093105d25823ca6c9b0d0
darcros <darcros7@gmail.com>
2024-01-05 19:54:04 +0100

    consegna 6

---
+++ src/routes/consegne/compito6/+page.svelte
@@ -0,0 +1,46 @@
+<script>
+	import { base } from '$app/paths';
+	import Prose from '$lib/Prose.svelte';
+	import PdfAttachment from '$lib/PdfAttachment.svelte';
+
+	import Content from './Content_06.md';
+</script>
+
+<Prose>
+	<Content />
+</Prose>
+
+<div class="container mx-auto my-8 px-8">
+	<PdfAttachment
+		src="{base}/consegne/compito6/Utenti Scontenti - Presentazione compito 6.pdf"
+		fileName="Utenti Scontenti - Presentazione compito 6.pdf"
+	/>
+</div>
+
+<div class="container mx-auto my-8 px-8">
+	<PdfAttachment
+		src="{base}/consegne/compito6/Utenti Scontenti - Questionario anagrafico.pdf"
+		fileName="Utenti Scontenti - Questionario anagrafico.pdf"
+	/>
+</div>
+
+<div class="container mx-auto my-8 px-8">
+	<PdfAttachment
+		src="{base}/consegne/compito6/Utenti Scontenti - Script.pdf"
+		fileName="Utenti Scontenti - Script.pdf"
+	/>
+</div>
+
+<div class="container mx-auto my-8 px-8">
+	<PdfAttachment
+		src="{base}/consegne/compito6/Utenti Scontenti - Indici di successo.pdf"
+		fileName="Utenti Scontenti - Indici di successo.pdf"
+	/>
+</div>
+
+<div class="container mx-auto my-8 px-8">
+	<PdfAttachment
+		src="{base}/consegne/compito6/Utenti Scontenti - Report correzioni.pdf"
+		fileName="Utenti Scontenti - Report correzioni.pdf"
+	/>
+</div>
---
+++ src/routes/consegne/compito6/Content_06.md
@@ -0,0 +1,13 @@
+# Consegna 6: Usability Testing e Prototipo High Fidelity
+
+Link ai protipi finali:
+* [app per gli utenti](https://www.figma.com/file/UtT6J5iYG28s5QoHHWg1Ft/Prototipo-App---Finale?type=design&node-id=0%3A1&mode=design&t=0hReo74JAA38quXP-1)
+* [webapp per lo staff](https://www.figma.com/file/PvS2Dqjjw9kSbOMAVSGzWj/Prototipo-WebApp---Finale?type=design&node-id=0%3A1&mode=design&t=TgpntQE7Ovbjmf1H-1)
+
+Alleghiamo qui sotto:
+* documenti relativi allo user testing:
+  * presentazione su procedura, risultati e raccomandazioni finali
+  * questionario anagrafico
+  * script
+  * tabella indici di successo
+* report sulle modifiche apportate al protitipo finale in seguito alla valutazione euristica
